4989ac331ac05c6dd68ceaf5f36495675e54a035
reza-tanha <adamak.tnh@gmail.com>
2022-06-03 19:54:39 +0430

    add view api order delete

--- config/api/urls.py
+++ config/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import books, BookDetail, orders, OrderDetail, order_create
+from .views import books, BookDetail, orders, OrderDetail, order_create, order_delete
 app_name = "api"
 
 urlpatterns = [
@@ -8,6 +8,8 @@ urlpatterns = [
 
     path('orders', orders, name="orders"),
     path('order/add', order_create, name="order-add"),
+    path('order/delete/<int:pk>', order_delete, name="order-delete"),
+    
     # path('orders', OrderDetail.as_view(), name="orders"),
     # path('<int:pk>', BookDetail.as_view(), name="book"),
 
--- config/api/views.py
+++ config/api/views.py
@@ -1,4 +1,3 @@
-from ast import Or
 from django.shortcuts import render
 from .serializer import BookSerializer, OrderSerializer
 from rest_framework.decorators import api_view
@@ -38,10 +37,14 @@ def orders(request):
 @api_view(['POST'])
 def order_create(request):
     order = OrderSerializer(data=request.data)
-    print(order)
-    if order.is_valid():
-        order.save()
-    return Response(order.data)
+    book = Book.objects.get(pk=request.data.get("book"))
+    if not book.reserve:
+        if order.is_valid():
+            book.reserve=True
+            book.save()
+            order.save()
+        return Response(order.data)
+    return Response("book exist")
     """
         {
             "is_reserve": true,
@@ -50,20 +53,16 @@ def order_create(request):
         }
     """
 
-@api_view(['POST'])
+@api_view(['DELETE'])
 def order_delete(request, pk):
-    order
-    order = OrderSerializer(data=request.data)
-    print(order)
-    if order.is_valid():
+    order = Order.objects.get(user=request.user, pk=pk)
+    if order:
+        book = Book.objects.get(pk=order.book.pk)
+        book.reserve=False
+        book.save()
+        order.is_reserve=False
         order.save()
-    return Response(order.data)
-    """
-        {
-            "is_reserve": true,
-            "user": 1,
-            "book": 5
-        }
-    """
+        return Response("order Deleted")
+
 
 
