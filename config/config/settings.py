753a490e3244ca968385c9b111b1984c9d6b6dbb
reza-tanha <adamak.tnh@gmail.com>
2022-06-08 14:56:16 +0430

    fix error Template

--- config/config/settings.py
+++ config/config/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
